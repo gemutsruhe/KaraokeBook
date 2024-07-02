8b8c9a27de05b5395e4574954c21a27f57f40281
gemutsruhe <evilparan@gmail.com>
2024-07-03 07:49:42 +0900

    feat: add popular type button listener

--- app/src/main/java/com/karaoke/karaokebook/view/PopularFragment.java
+++ app/src/main/java/com/karaoke/karaokebook/view/PopularFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -18,23 +19,31 @@ public class PopularFragment extends Fragment {
     PopularChartRepository popularChartRepository;
     PopularChartViewModel popularChartViewModel;
     FragmentPopularBinding binding;
-    List<PopularSongListLayout> popularLayoutList = new ArrayList<>(1);
+    ArrayList<Button> typeButtonList = new ArrayList<>();
+    List<PopularSongListLayout> popularLayoutList = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentPopularBinding.inflate(getLayoutInflater(), container, false);
         popularChartRepository = PopularChartRepository.getInstance();
         popularChartViewModel = new PopularChartViewModel();
+
         initPopularList();
 
-        binding.japanPopularBtn.setOnClickListener(view -> {
-            changeType(2);
-        });
+        typeButtonList.add(binding.koreanPopularBtn);
+        typeButtonList.add(binding.popPopularBtn);
+        typeButtonList.add(binding.japanPopularBtn);
+
+        for(int i = 0; i < typeButtonList.size(); i++) {
+            int type = i;
+            typeButtonList.get(type).setOnClickListener(view -> changeType(type));
+        }
 
         popularChartViewModel.getPopularList();
 
         for(int i = 0; i <= 2; i++) {
             int type = i;
+
             popularChartRepository.getPopularList(type).observe(getViewLifecycleOwner(), cellDataList -> {
                 popularLayoutList.get(type).addPopularList(cellDataList);
                 if(type == 0) {
@@ -43,15 +52,17 @@ public class PopularFragment extends Fragment {
             });
         }
 
-        changeType(0);
         return binding.getRoot();
     }
 
     private void initPopularList() {
         for(int type = 0; type <= 2; type++) {
             popularLayoutList.add(new PopularSongListLayout(getContext()));
-            //popularLayoutList.get(type).addPopularList(type);
         }
+    }
+
+    private void addButtonList() {
+
     }
 
     private void changeType(int type) {
